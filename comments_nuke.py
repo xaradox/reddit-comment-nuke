eaedb2b962284f9e9147be173962305160c87df0
Robert Day <robd098@gmail.com>
2023-06-11 01:21:48 +0100

    - Added functionality to restore original comments from local save - Minor changes to improve efficiency

--- comments_nuke.py
+++ comments_nuke.py
@@ -16,28 +16,53 @@ def auth_user(user_client_id, user_client_secret, user_username, user_password):
     )
     reddit.validate_on_submit = True
 
-    user = reddit.user.me()
-    if user == user_username:
-        return user
-    else:
-        return None
+    return reddit
 
+# Limted by reddit API to 1000 comments
 def get_comments(user):
     return user.comments.new(limit=None)
 
-def save_comments(comments):
+def save_comments(user):
     from datetime import datetime
 
+    # Get user comment history
+    comments = get_comments(user)
+
+    data = []
+    # Append data from each comment to JSON object
+    for comment in comments:
+        comment_dict = {"comment_id" : comment.id,
+                        "date" : datetime.utcfromtimestamp(comment.created_utc).strftime('%Y/%m/%d'),
+                        "subreddit" : comment.subreddit.display_name,
+                        "comment_body" : comment.body}
+        data.append(comment_dict)
+
+    # Write the entire object to file
     with open('comments.json','w',encoding='utf-8') as f_comments:
-        data = []
-        for comment in comments:
-            comment_dict = {"comment_id" : comment.id,
-                            "date" : datetime.utcfromtimestamp(comment.created_utc).strftime('%Y/%m/%d'),
-                            "subreddit" : comment.subreddit.display_name,
-                            "comment_body" : comment.body}
-            data.append(comment_dict)
         json.dump(data, f_comments, indent=4, ensure_ascii=False)
 
+def restore_comments(reddit):
+    with open('comments.json','r',encoding='utf-8') as f_comments:
+        original_comments = json.load(f_comments)
+    
+    for orig_comment in original_comments:
+        reddit.comment(orig_comment['comment_id']).edit(orig_comment['comment_body'])
+
+def edit_comments(user, key):
+    # Get user comment history
+    comments = get_comments(user)
+
+    for comment in comments:
+        if ('ID=' + comment.id) not in comment.body:
+            # Encrypt the comment body with the user key
+            encrypted_comment = encrypt_text(comment.body.encode(), key)
+            # Fix encoding of comment ciphertext
+            encrypted_comment = b64encode(encrypted_comment).decode()
+            # Edit the comment according to 'replace_with' and insert ciphertext 
+            comment.edit(replace_with.format(comment.id, encrypted_comment))
+
+# Relevant documentation: 
+# https://cryptography.io/en/latest/hazmat/primitives/aead/#cryptography.hazmat.primitives.ciphers.aead.AESGCM
 def encrypt_text(text, key):
     nonce = secrets.token_bytes(12)
     return nonce + AESGCM(key).encrypt(nonce, text, b"")
@@ -46,8 +71,7 @@ def decrypt_text(text, key):
     return AESGCM(key).decrypt(text[:12], text[12:], b"")
 
 # All comments are edited according to this string
-replace_with = """
-*This comment has been edited in protest to reddit's API policy changes, their treatment of developers of 3rd party apps, and their response to community backlash.*  
+replace_with = """*This comment has been edited in protest to reddit's API policy changes, their treatment of developers of 3rd party apps, and their response to community backlash.*  
   
 &nbsp;  
 [Details of the end of the Apollo app](https://old.reddit.com/r/apolloapp/comments/144f6xm/apollo_will_close_down_on_june_30th_reddits/)  
@@ -89,37 +113,28 @@ def main(self):
 
     # Users AES-GCM secret key 
     key = credentials['encryption_key']
+    # Key needs to be in bytes format
+    key = b64decode(key.encode())
 
-    # Authenticate user
-    user = auth_user(
+    # Attempt to authenticate user
+    reddit = auth_user(
         credentials['client_id'],
         credentials['client_secret'],
         credentials['username'],
         credentials['password'],
     )
 
-    if user == None:
+    # Ensure authentication succeeded by checking username matches
+    user = reddit.user.me()
+    if user != credentials['username']:
         raise Exception('Error: Failed to authenticate user')
 
-    # Restore comments to their original content
-    # Could attempt to restore from local copy if it exists?
-    # Otherwise extract ciphertext from user comments and decrypt
-    if args['restore_comments']:
-        raise NotImplementedError()
-
-    comments = get_comments(user)
-
     if args['save_locally']:
-        save_comments(comments)
-
-    for comment in comments:
-        if ('ID=' + comment.id) not in comment.body:
-            # Encrypt the comment body with the user key
-            encrypted_comment = encrypt_text(comment.body.encode(), key)
-            # Fix encoding of comment ciphertext
-            encrypted_comment = b64encode(encrypted_comment).decode()
-            # Edit the comment according to 'replace_with' and insert ciphertext 
-            comment.edit(replace_with.format(comment.id, encrypted_comment))
+        save_comments(user)
+    elif args['restore_comments']:
+        restore_comments(reddit)
+    else:
+        edit_comments(user, key)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
